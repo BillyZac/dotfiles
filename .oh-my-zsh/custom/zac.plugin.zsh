2ed407d87b34fb7ab389d548a9d44d2b28a2bab9
Zachary Smith <william.smith1@wipro.com>
2016-09-14 10:25:18 -0400

    Add cd into Synapse

--- .oh-my-zsh/custom/zac.plugin.zsh
+++ .oh-my-zsh/custom/zac.plugin.zsh
@@ -2,6 +2,9 @@ alias hi="echo hulloh."
 
 # Synapse project
 
+# cd into Synapse front-end
+alias s="cd $HOME/dev/synapse"
+
 # Synapse front-end
 alias sf="echo '====================' && echo ENTER THE SYNAPSE!!! && echo '====================' && echo && echo && cd /Users/zacharysmith/dev/synapse/ && atom . && git checkout master && git pull && npm install && npm run build && npm start"
 
@@ -10,3 +13,6 @@ alias sb="cd /Users/zacharysmith/dev/midas-project/mi_rest_hack/ && npm install 
 
 # Fire up Mongo, which back-end needs real bad
 alias m="mongod"
+
+# git
+alias gl="git log --oneline -n 10"
