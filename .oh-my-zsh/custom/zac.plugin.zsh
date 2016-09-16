1cf5ac42a81b11b8ce026c84f5d52202edfbc478
Zachary Smith <william.smith1@wipro.com>
2016-09-16 10:44:05 -0400

    Make Synapse scripts generic. Or more generic, anyways.

--- .oh-my-zsh/custom/zac.plugin.zsh
+++ .oh-my-zsh/custom/zac.plugin.zsh
@@ -1,15 +1,13 @@
-alias hi="echo hulloh."
-
 # Synapse project
 
 # cd into Synapse front-end
-alias s="cd $HOME/dev/synapse"
+alias s="cd $HOME/work/synapse"
 
 # Synapse front-end
-alias sf="echo '====================' && echo ENTER THE SYNAPSE!!! && echo '====================' && echo && echo && cd /Users/zacharysmith/dev/synapse/ && atom . && git checkout master && git pull && npm install && npm run build && npm start"
+alias sf="echo '====================' && echo ENTER THE SYNAPSE!!! && echo '====================' && echo && echo && cd $HOME/work/synapse/ && atom . && git checkout master && git pull && npm install && npm run build && npm start"
 
 # Synapse back-end
-alias sb="cd /Users/zacharysmith/dev/midas-project/mi_rest_hack/ && npm install && node ./app/api.js"
+alias sb="cd $HOME/work/mi_rest_hack/ && npm install && node ./app/api.js"
 
 # Fire up Mongo, which back-end needs real bad
 alias m="mongod"
