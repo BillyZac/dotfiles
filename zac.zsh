823fa5b416b7d9333d7f6a6c702a07632a94d1ed
Zachary Smith <william.smith1@wipro.com>
2017-08-23 10:32:19 -0400

    Update aliases
    - Remove Synapse project
    - Refine Bookit project
    - Remove git log (using `glo` from Oh My Zsh git plugin instead)

--- zac.zsh
+++ zac.zsh
@@ -2,30 +2,17 @@
 alias a="atom ."
 alias v="vim ."
 
-# ======= Synapse project
-
-# cd into Synapse front-end
-alias s="cd $HOME/work/synapse"
-
-# Synapse front-end
-alias sf="echo '====================' && echo ENTER THE SYNAPSE!!! && echo '====================' && echo && echo && cd $HOME/work/synapse/ && git checkout master && git pull && npm install && npm run build && npm start"
-
-# Synapse back-end
-alias sb="cd $HOME/work/eolas/ && npm install && node ./index.js"
-
-
 
 # ======= Bookit project
 alias bf="cd $HOME/work/bookit-web"
 alias bs="cd $HOME/work/bookit-server"
+alias bfr="yarn start"
+alias bsr="npm run build && npm run server" #Run
 
 
-
-# Fire up Mongo, which the server needs in the worst way
+# Fire up Mongo
 alias m="mongod"
 
-# git
-alias gl="git log --oneline -n 20"
 
 # clear the screen, esp useful in tmux, where cmd+k doesn't play nice with panes
 alias c="clear"
